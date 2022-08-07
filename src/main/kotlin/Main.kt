54072db87acd279615a43b186d2f07e4cc87d7a8
khaledhassssan <engkhaledhassan19@gmail.com>
2022-08-07 15:58:07 +0300

    Version 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,8 +9,12 @@ fun main() {
     var second = (1..10).shuffled().first();
     println("Second number = "+second);
 
+    // Third number
+    var third = (1..10).shuffled().first();
+    println("Third number = "+third);
+
     // Sum operation
-    var sum = first + second ;
+    var sum = first + second +third;
     // Print result
     println("Sum of three Random Numbers is "+sum);
 }
